5fe2c60378fd227421773037e083d1cf3229b0c0
mohamedS-N10015122 <salad2692@gmail.com>
2026-01-28 21:28:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("First part:");
+
+let studentName = "Dan";
+let studentAge = 18;
+isEnrolled = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+console.log("Second part:");
+
+let String1 = "Hello World!"
+let Number1 = 54;
+let Boolean1 = true;
+let Array1 = ["One", "Two", "Three"];
+let Object1 = { isEating: true , isPlaying: false};
+
+console.log(String1);
+console.log(Number1);
+console.log(Boolean1);
+console.log(Array1);
+console.log(Object1);
+
+console.log("Third part:");
+let x = 5
+let y = 8
+console.log("x times y is " + x*y);
+
+if (x<y) {
+    console.log("x is less then y");
+}
+
+let IsJumping = true;
+let IsWalking = false;
+
+console.log("IsWalking (true) && IsJumping (false) is");
+console.log(IsWalking && IsJumping);
+
